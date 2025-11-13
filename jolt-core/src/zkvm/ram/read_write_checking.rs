8282b7121b713baf95c5406878e33c80afb996c9
Michael Zhu <mchl.zhu.96@gmail.com>
2025-11-13 11:29:36 -0500

    Mergesort-like binding algorithm for SparseValPolynomial

--- jolt-core/src/zkvm/ram/read_write_checking.rs
+++ jolt-core/src/zkvm/ram/read_write_checking.rs
@@ -358,7 +358,7 @@ impl<F: JoltField> RamReadWriteCheckingProver<F> {
             })
             .collect();
 
-        let sparse_val = SparseValPolynomial::new(&trace, &preprocessing.memory_layout, params.K);
+        let sparse_val = SparseValPolynomial::new(&trace, &preprocessing.memory_layout);
 
         Self {
             ram_addresses,
